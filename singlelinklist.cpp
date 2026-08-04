8f14e891f27f4d325865451dd15b89c8dde5b7ca
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-08-04 15:08:16 +0700

    memasukkan booldenode

--- singlelinklist.cpp
+++ singlelinklist.cpp
@@ -76,3 +76,18 @@ public:
 
         return (*current != NULL);
     }
+
+    bool deNode(int nim)
+    {
+        Node *current, *previous;
+        if (Search(nim, &previous, &current) == false)
+            return false;
+
+        if (current == START)
+            START = START->next;
+        else
+            previous->next = current->next;
+
+        delete current;
+        return true;
+    }
